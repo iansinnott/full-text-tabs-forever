48fa5876e78b19c9de1d0abd584e2ea26e661b5c
Ian Sinnott <ian@iansinnott.com>
2023-01-30 01:15:39 +0800

    wait for page load

--- src/background.ts
+++ src/background.ts
@@ -6,35 +6,44 @@ type VisitOpts = {
   previewImageBase64?: string;
 };
 
-const handleVisit = async (url?: string, title?: string, opts: VisitOpts = {}) => {
+const handleVisit = async (tab: chrome.tabs.Tab, opts: VisitOpts = {}) => {
+  if (tab.url === undefined) return;
+  if (tab.url.startsWith("moz-extension://")) return;
+  if (tab.url.startsWith("about:")) return;
+  if (tab.url.startsWith("data:")) return;
+  if (tab.url.includes("localhost:")) return;
+
+  let fancyUrl = "";
+  try {
+    fancyUrl = new URL(tab.url).hostname;
+  } catch (err) {
+    console.log("No URL to parse");
+  }
+
   const { type } = opts;
-  console.log(type, url, title);
+  console.log(`%c${type}`, "color:salmon;", fancyUrl, tab.title);
 };
 
 chrome.runtime.onInstalled.addListener(async () => {
-  log("background", greetings);
+  log("background installed:", greetings);
 });
 
 // @note It is usually the case that a new tab displays about:blank as the URL
 // before it is updated. Thus, see the on updated handler below.
-chrome.tabs.onCreated.addListener((tab) => {
+chrome.tabs.onCreated.addListener(async (tab) => {
   if (tab.url === undefined) return;
   if (tab.url.startsWith("moz-extension://")) return;
   if (tab.url.startsWith("about:")) return;
 
-  handleVisit(tab.url, tab.title, {
+  await handleVisit(tab, {
     type: "created",
   });
 });
 
 // whenever a tab is updated, log the URL
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (tab.url === undefined) return;
-  if (tab.url.startsWith("moz-extension://")) return;
-  if (tab.url.startsWith("about:")) return;
-  if (tab.url.startsWith("data:")) return;
-  if (tab.url.includes("localhost:")) return;
   if (changeInfo.status === "loading") return;
+
   // const previewImageOpts = {
   //   format: "jpeg" as const,
   //   quality: 40,
@@ -42,7 +51,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   // const previewImageBase64 = await chrome.tabs.captureVisibleTab(tab.windowId, previewImageOpts);
   // console.log("preview opts:", previewImageOpts);
 
-  handleVisit(tab.url, tab.title, {
+  await handleVisit(tab, {
     type: "updated",
   });
 });
@@ -51,7 +60,22 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
   const tab = await chrome.tabs.get(activeInfo.tabId);
 
-  handleVisit(tab.url, tab.title, {
+  await handleVisit(tab, {
     type: "activated",
   });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  try {
+    const { tab } = sender;
+    const [type, payload] = request as [string, any];
+    console.log(`%c${type}`, "color:lime;", { url: tab?.url, payload });
+    if (sender.url !== tab?.url) {
+      console.log(`%cinfo`, "color:yellow;", "sender URL and tab URL differ. probably iframe");
+    }
+    sendResponse({ jello: "yes" });
+  } catch (err) {
+    console.error("Could not parse message", request, sender, err);
+    sendResponse({ error: err.message });
+  }
 });
--- src/content-scripts/content-script.ts
+++ src/content-scripts/content-script.ts
@@ -1,4 +1,37 @@
 import { greetings } from "../common/const";
 import { log } from "../common/logs";
 
-log("content-script", greetings);
+const main = async () => {
+  log("Ping...");
+
+  const res = await chrome.runtime.sendMessage([
+    "getPageStatus",
+    {
+      url: window.location.href,
+    },
+  ]);
+
+  log("...Pong", res);
+};
+
+// Plumbing
+(async () => {
+  // check if dom content has already been loaded
+  if (document.readyState !== "complete") {
+    console.log("%cwait for ready", "color:yellow;font-size:12px;", document.readyState);
+
+    // @note This can take _a while_, but is in place to account for apps that
+    // may not have built the dom yet
+    await new Promise((resolve) => {
+      document.onreadystatechange = () => {
+        if (document.readyState === "complete") {
+          resolve(null);
+        }
+      };
+    });
+
+    console.log("%cready.", "color:green;font-size:12px;", document.readyState);
+  }
+
+  await main();
+})();
