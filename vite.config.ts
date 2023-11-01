aaaea49f1f1702515e530f0c1e6b224f6adbd22a
Ian Sinnott <ian@iansinnott.com>
2023-11-01 22:07:37 +0800

    configure vite to avoid old plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import path from "node:path";
+import fs from "node:fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       // Vite was not ignoring tmp dir, which is the only reason I added this
-      input: ["index.html"],
+      input: {
+        index: "index.html",
+        background: "src/background.ts",
+        "content-scripts/content-script": "src/content-scripts/content-script.ts",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
     },
-    minify: process.env.NODE_ENV !== "development",
+    minify: false,
     emptyOutDir: false,
   },
   server: {
@@ -18,11 +27,32 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    exclude: ["@sqlite.org/sqlite-wasm"],
+    exclude: ["@sqlite.org/sqlite-wasm", "@vlcn.io/crsqlite-wasm"],
   },
   plugins: [
     svelte({
       preprocess: vitePreprocess(),
     }),
+    {
+      name: "copy-plugin",
+      apply: "build",
+      enforce: "post",
+      generateBundle() {
+        const sourceDir = path.resolve(__dirname, "src/assets");
+        const destinationDir = path.resolve(__dirname, "dist/assets");
+
+        fs.mkdirSync(destinationDir, { recursive: true });
+
+        const files = fs.readdirSync(sourceDir);
+
+        for (const filepath of files) {
+          const sourcePath = path.join(sourceDir, filepath);
+          const destinationPath = path.join(destinationDir, filepath);
+
+          fs.copyFileSync(sourcePath, destinationPath);
+          console.log(`[copy-plugin] ${sourcePath} -> ${destinationPath}`);
+        }
+      },
+    },
   ],
 });
