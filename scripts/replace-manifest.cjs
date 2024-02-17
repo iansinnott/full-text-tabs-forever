26932526ab7eb51d9f8190984d45497b165fcfc4
Nicholas Mullikin <17802256+nicholasmullikin@users.noreply.github.com>
2024-02-17 15:13:43 -0500

    Add more build options

--- scripts/replace-manifest.cjs
+++ scripts/replace-manifest.cjs
@@ -9,6 +9,20 @@ const modifyManifest = (manifest) => {
   delete manifest["$schema"];
 };
 
+// const args = process.argv;
+// let platform = "chrome";
+// if (args.length > 1) {
+//     console.error("More than 1 argument specified.");
+// }
+
+// if (args.length === 1) {
+//   if (args[0] === "chrome") {
+//     platform = "chrome";
+//   } else {
+//     platform = "firefox";
+//   }
+// }
+
 try {
   const manifestV3 = JSON.parse(
     readFileSync(path.resolve(__dirname, "../dist/manifest.json"), "utf8")
@@ -22,7 +36,7 @@ try {
     JSON.stringify(manifestV3, null, 2)
   );
 
-  console.log("Manifest converted v3 -> v2");
+  console.log("Manifest cleaned");
 } catch (err) {
   console.error("Could not build manifest", err);
 }
