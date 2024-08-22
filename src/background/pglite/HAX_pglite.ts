50d574373b6a07de812175f2bda590d0be601277
Ian Sinnott <3154865+iansinnott@users.noreply.github.com>
2024-08-22 23:18:15 +0800

    make pglite hax slightly cleaner

--- src/background/pglite/HAX_pglite.ts
+++ src/background/pglite/HAX_pglite.ts
@@ -48,37 +48,23 @@ class ProgressEventPolyfill {
 // A partial polyfill for XMLHttpRequest to support the loading of pglite in a
 // service worker
 class XMLHttpRequestPolyfill {
-  private method: string = "";
   private url: string = "";
-  private headers: { [key: string]: string } = {};
-  private body: any = null;
   public onload: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null = null;
   public onerror: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null = null;
   public status: number = 0;
   public responseText: string = "";
   public response: any = null;
-  public responseType: XMLHttpRequestResponseType = "";
 
   open(method: string, url: string) {
     console.log("open ::", { method, url });
-    this.method = method;
     this.url = url;
-  }
-
-  setRequestHeader(name: string, value: string) {
-    console.log("setRequestHeader ::", { name, value });
-    this.headers[name] = value;
   }
 
   send(body: any = null) {
     console.log("send ::", { body });
-    this.body = body;
     if (assetCache.has(this.url)) {
       this.response = assetCache.get(this.url);
       this.status = 200;
-      if (this.responseType === "text") {
-        this.responseText = new TextDecoder().decode(this.response);
-      }
       if (this.onload) {
         // @ts-expect-error
         this.onload.call(this, new ProgressEventPolyfill("load") as any);
