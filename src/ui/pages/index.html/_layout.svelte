0cd1a4f56edd05bfec35f1dae58d889066f317b6
Ian Sinnott <3154865+iansinnott@users.noreply.github.com>
2024-08-26 11:05:28 +0800

    add links to routes

--- src/ui/pages/index.html/_layout.svelte
+++ src/ui/pages/index.html/_layout.svelte
@@ -4,6 +4,8 @@
   import Menu from "@/ui/Menu.svelte";
   import { onMount, tick } from "svelte";
   import { fttf } from "@/ui/lib/rpc";
+  import { routes } from "@/ui/.routify/routes";
+  import { toLabel } from "@/common/utils";
 
   onMount(() => {
     if (typeof window !== "undefined") {
@@ -19,6 +21,17 @@
       });
     }
   };
+
+  const routeLabels = {
+    index: "Search",
+    "database-repl": "Database REPL",
+    settings: "Settings",
+  };
+
+  const navigationRoutes = [
+    routes.find((route) => route.name === "index"), // Search page first
+    ...routes.filter((route) => route.name !== "index"),
+  ].filter(Boolean);
 </script>
 
 <svelte:window
@@ -45,12 +58,40 @@
   />
 {/if}
 
-<button
-  class="fixed bottom-4 left-4 z-10 p-2 rounded-full bg-zinc-900 border border-zinc-600"
-  on:click={handleCmdK}
->
-  <Cog />
-</button>
+<div class="LowerMenu">
+  <nav class="flex space-x-2 overflow-x-auto rounded bg-zinc-900 border border-zinc-600 text-white">
+    {#each navigationRoutes as route}
+      <a
+        href={"#" + route.path}
+        class="flex-shrink-0 px-4 py-2 hover:bg-zinc-800 whitespace-nowrap items-center flex"
+      >
+        {routeLabels[route.name] || toLabel(route.name)}
+      </a>
+    {/each}
+  </nav>
+  <button
+    class="flex-shrink-0 p-2 rounded bg-zinc-900 border border-zinc-600 w-11 h-11 hover:bg-zinc-800"
+    on:click={handleCmdK}
+  >
+    <Cog />
+  </button>
+</div>
 
 <style>
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  nav::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge and Firefox */
+  nav {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+  .LowerMenu {
+    @apply fixed bottom-4 left-4 z-10 max-w-[calc(100%-2rem)] w-full;
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: 0.5rem;
+  }
 </style>
