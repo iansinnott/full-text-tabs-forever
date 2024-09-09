f9a4a3ea1dec6f7280a7abc87ade3703cd411e4c
Ian Sinnott <3154865+iansinnott@users.noreply.github.com>
2024-09-09 15:44:12 +0800

    make database repl a bit more user friendly

--- src/ui/pages/index.html/database-repl.svelte
+++ src/ui/pages/index.html/database-repl.svelte
@@ -5,22 +5,11 @@
   import { rpc } from "@/ui/lib/rpc";
 
   let replElement: HTMLElement & { pg: PGlite };
-
-  class PGliteServiceWorkerProxy implements Pick<PGlite, "query" | "exec" | "waitReady"> {
-    async query<T>(query: string, params?: any[], options?: QueryOptions): Promise<Results<T>> {
-      const result = await rpc(["pg.query", { sql: query, params, options }]);
-      return result as Results<T>;
-    }
-
-    async exec(query: string, options?: QueryOptions): Promise<Array<Results>> {
-      const result = await rpc(["pg.exec", { sql: query, options }]);
-      return result as Array<Results>;
-    }
-
-    waitReady = Promise.resolve();
-  }
+  let showInstructions: boolean;
 
   onMount(async () => {
+    showInstructions = localStorage.getItem("showInstructions") !== "false";
+
     // @ts-expect-error Types are wrong for pglite for some reason
     await import("@electric-sql/pglite-repl/webcomponent");
 
@@ -41,9 +30,47 @@
       replElement.pg = pgproxy;
     }
   });
+
+  function toggleInstructions() {
+    showInstructions = !showInstructions;
+    localStorage.setItem("showInstructions", showInstructions.toString());
+  }
+
+  class PGliteServiceWorkerProxy implements Pick<PGlite, "query" | "exec" | "waitReady"> {
+    async query<T>(query: string, params?: any[], options?: QueryOptions): Promise<Results<T>> {
+      const result = await rpc(["pg.query", { sql: query, params, options }]);
+      return result as Results<T>;
+    }
+
+    async exec(query: string, options?: QueryOptions): Promise<Array<Results>> {
+      const result = await rpc(["pg.exec", { sql: query, options }]);
+      return result as Array<Results>;
+    }
+
+    waitReady = Promise.resolve();
+  }
 </script>
 
 <div class="flex flex-col p-4 h-[calc(100%-70px)]">
+  <button
+    on:click={toggleInstructions}
+    class="self-end mb-2 px-3 py-1 bg-gray-700 rounded-md text-sm"
+  >
+    {showInstructions ? "Hide" : "Show"} Instructions
+  </button>
+
+  {#if showInstructions}
+    <div class="Instructions prose prose-invert mb-8">
+      <h2>SQL Playground</h2>
+      <p>
+        You can directly access the browsing history database here. If you're not familiar with SQL,
+        ChatGPT can likely help you.
+      </p>
+      <p>Here's an example query that shows the 10 most recently visited documents:</p>
+      <pre>SELECT * FROM document order by last_visit desc limit 10;</pre>
+    </div>
+  {/if}
+
   <pglite-repl bind:this={replElement}></pglite-repl>
 </div>
 
